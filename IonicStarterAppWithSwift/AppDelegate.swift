6b3fa392d4ac87aa3023ff1f19483e619fd432e0
Bhuman Soni <bhuman@yourdiagrams.com>
2015-02-09 17:33:36 +1100

    add the logic to get user permissions on iOS versions 8 and above

--- IonicStarterAppWithSwift/AppDelegate.swift
+++ IonicStarterAppWithSwift/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        if(UIApplication.instancesRespondToSelector(Selector("registerUserNotificationSettings:"))) {
+            UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: .Alert | .Badge, categories: nil))
+        }
         return true
     }
 
